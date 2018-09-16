eda6d053c2b779950e5ad37fd8e7ad6c98540081
appsgithub <ejerciciosapps@gmail.com>
2018-09-15 19:00:58 -0500

    change guestbook

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/a7da03ba925b25620bb2463f1a8c9c8b8c53c267c9cd059f653191979e32c6bb/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
